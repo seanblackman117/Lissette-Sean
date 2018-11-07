621fcb0bdd752ff5cd8f6f8d7c17af67ce308967
lissette duran <lissette0247@gmail.com>
2018-11-07 18:29:13 -0500

    added for loop to interate through the data

--- js/index.js
+++ js/index.js
@@ -4,14 +4,18 @@
 // console.log('test');
 
 // test();
-import {getValues} from './data.js'
-getValues();
-// var database = firebase.database();
-// var databaseRef = database.ref('/');
+//import {getValues} from './data.js'
+//getValues();
+ var database = firebase.database();
+var databaseRef = database.ref('/');
 var values = databaseRef.once('value').then(function(snapshot) {
     const databaseValues = snapshot.val();
     console.log(databaseValues);
-    $("body").html(databaseValues.Blogs["blog 1 "].body);
+    for(var key in databaseValues.Blogs){
+        $("body").append(databaseValues.Blogs[key].body);
+        $("body").append("<img src=" +  databaseValues.Blogs[key].image +" >");
+        $("body").append("<h1> "+ databaseValues.Blogs[key].title + "</h1>");
+    }
    // return databaseValues;
 
 });
