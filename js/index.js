1cded9ab00a6a68f79ed22a661999ae26add230a
lissette duran <lissette0247@gmail.com>
2018-11-14 18:24:22 -0500

    added css and input for firebase

--- js/index.js
+++ js/index.js
@@ -12,12 +12,25 @@ var values = databaseRef.once('value').then(function(snapshot) {
     const databaseValues = snapshot.val();
     console.log(databaseValues);
     for(var key in databaseValues.Blogs){
-        $("body").append(databaseValues.Blogs[key].body);
-        $("body").append("<img src=" +  databaseValues.Blogs[key].image +" >");
-        $("body").append("<h1> "+ databaseValues.Blogs[key].title + "</h1>");
+        $("#main").append("<h1> "+ databaseValues.Blogs[key].title + "</h1>" +"<br>");
+        $("#main").append("<img class='imgs' src=" +  databaseValues.Blogs[key].image +" >"+"<br>");
+        $("#main").append("<p class= 'text'>" + databaseValues.Blogs[key].body +"</p>" +"<br>");
+
     }
    // return databaseValues;
 
 });
+
+$("#submit").click(function(){
+    var title= $("#title").val();
+    var text= $("#text").val();
+    var image= $("#image").val();
+    var blogRef=database.ref('/Blogs');
+    blogRef.push({
+        body: text,
+        image: image,
+        title: title,
+    });
+})
 
 //console.log("what is this value?", values);
