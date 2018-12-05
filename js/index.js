72a6dc54f6b944a72c6d8b7c4dd497c147bfcc37
lissette duran <lissette0247@gmail.com>
2018-12-05 17:53:39 -0500

    added bootstrap

--- js/index.js
+++ js/index.js
@@ -12,9 +12,25 @@ var values = databaseRef.once('value').then(function(snapshot) {
     const databaseValues = snapshot.val();
     console.log(databaseValues);
     for(var key in databaseValues.Blogs){
-        $("#main").append("<h1> "+ databaseValues.Blogs[key].title + "</h1>" +"<br>");
-        $("#main").append("<img class='imgs' src=" +  databaseValues.Blogs[key].image +" >"+"<br>");
-        $("#main").append("<p class= 'text'>" + databaseValues.Blogs[key].body +"</p>" +"<br>");
+        $("#main").append(
+            '<div class="col-md-6">' +
+            '<div class="card flex-md-row mb-4 shadow-sm h-md-250">'+
+              '<div class="card-body d-flex flex-column align-items-start">'+
+                // '<strong class="d-inline-block mb-2 text-primary">World</strong>'+
+                '<h3 class="mb-0">'+
+                  '<a class="text-dark" href="#">'+ databaseValues.Blogs[key].title +'</a>'+
+                '</h3>'+
+                // '<div class="mb-1 text-muted">Nov 12</div>'+
+                '<p class="card-text mb-auto">'+ databaseValues.Blogs[key].body +'</p>'+
+                '<a href="#">Continue reading</a>'+
+              '</div>'+
+              '<img class="card-img-right flex-auto d-none d-lg-block" src='+  databaseValues.Blogs[key].image +' alt="Card image cap">'+
+            '</div>'+
+          '</div>'
+        )
+        // $("#main").append("<h1> "+ databaseValues.Blogs[key].title + "</h1>" +"<br>");
+        // $("#main").append("<img class='imgs' src=" +  databaseValues.Blogs[key].image +" >"+"<br>");
+        // $("#main").append("<p class= 'text'>" + databaseValues.Blogs[key].body +"</p>" +"<br>");
 
     }
    // return databaseValues;
